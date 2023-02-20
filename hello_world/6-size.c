19ea16134e202ddf8669466c970f5b21f29f71cd
SharifAli645 <pandemiaonirica@gmail.com>
2023-02-20 14:49:25 -0800

    C program that prints the size of various types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,13 +10,13 @@ int main(void)
 	int intType;
 	float floatType;
 	double doubleType;
-	char charType;
 	long int longintType;
 	long long int longlongintType;
+	char charType;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(longingType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 	return (0);
