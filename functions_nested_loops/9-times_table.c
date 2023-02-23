dbdef826a48d95c5aaa2942378f95f96a047a992
SharifAli645 <pandemiaonirica@gmail.com>
2023-02-23 08:55:25 -0800

    function that prints the 9 times table, starting with 0.

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,27 +1,36 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * times_table - write the table of the nine
+ * times_table - prints the table of nine
  *
- * Return: Always void
- */
+ * Return: Always nothing
+ **/
 void times_table(void)
 {
 	int i = 0;
 	int e = 0;
-	int res;
 
 	while (i < 10)
 	{
 		while (e < 10)
 		{
-			res = (i * e);
-			_putchar(res + '0');
-			_putchar(',');
-			_putchar(' ');
-			e = e + 1;
+			int r = i * e;
+
+			if (e == 0)
+			{
+				printf("%d,", r);
+			}
+			else if (e < 9)
+			{
+				printf("%3d,", r);
+			} else
+			{
+				printf("%3d", r);
+			}
+			e++;
 		}
-		i = i + 1;
-		_putchar(10);
+		printf("\n");
+		i++;
+		e = 0;
 	}
-	return;
 }
