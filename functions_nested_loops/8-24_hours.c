d35e398567774560be082cf8fbbd9a1e0181aac9
SharifAli645 <pandemiaonirica@gmail.com>
2023-02-22 18:54:39 -0800

    function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,12 +7,35 @@
  */
 void jack_bauer(void)
 {
-	for (int hora = 0; hora < 24; hora++)
+	int hora1 = 0;
+	int hora2 = 0;
+	int minuto1 = 0;
+	int minuto2 = 0;
+
+	while (hora1 < 3)
 	{
-		for (int minuto = 0; minuto < 60; minuto++)
+		while (hora2 < 10)
 		{
-			printf("%d:%d", hora, minuto);
-			printf("\n");
+			while (minuto1 < 6)
+			{
+				while (minuto2 < 10)
+				{
+					
+					if(hora1 == 2 && hora2 == 4 && minuto1 == 0 && minuto2 == 0)
+					{
+						return;
+					}
+					printf("%d%d:%d%d", hora1, hora2, minuto1, minuto2);
+					minuto2++;
+		                        printf("\n");
+				}
+				minuto2 = 0;
+				minuto1++;
+			}
+			minuto1 = 0;
+			hora2++;
 		}
+		hora2 = 0;
+		hora1++;
 	}
 }
