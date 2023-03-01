c7053607bcb7fd87f4e569637bf5eca0c7348aa8
SharifAli645 <pandemiaonirica@gmail.com>
2023-03-01 08:56:04 -0800

    function that copies the string pointed

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcpy - function that copies one source string to a array
+ * @dest: empty array
+ * @src: source string
+ *
+ * Return: One array
+ **/
+char *_strcpy(char *dest, char *src)
+{
+	char *ptr_src = src;
+	char *ptr_dest = dest;
+
+	while (*ptr_src != '\0')
+	{
+		*ptr_dest = *ptr_src;
+
+		ptr_src++;
+		ptr_dest++;
+	}
+
+	return (dest);
+
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ void print_array(int *a, int n);
 void puts2(char *str);
 void rev_string(char *s);
 void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
