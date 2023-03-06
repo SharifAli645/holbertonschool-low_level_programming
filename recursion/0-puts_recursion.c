0b9fd25149bb949518a2d17d48fa3cfbfa1fc1fd
SharifAli645 <pandemiaonirica@gmail.com>
2023-03-06 08:48:23 -0800

    function that prints a string

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer
+ *
+ * Return: Always a void
+ **/
+void _puts_recursion(char *s)
+{
+	char *ptr = s;
+
+	if (*ptr == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*ptr);
+	ptr++;
+	_puts_recursion(ptr);
+	return;
+}
