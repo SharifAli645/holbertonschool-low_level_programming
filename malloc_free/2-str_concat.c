a17b14edf8bb6707de2af4fab681a56bad1d1591
SharifAli645 <pandemiaonirica@gmail.com>
2023-03-16 09:15:13 -0700

    function that concatenates two strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,7 +16,6 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	char *arr;
 
-	arr = malloc(sizeof(char) * (size1 + size2 + 1));
 	if (arr == NULL)
 		return (NULL);
 	if (s2 == NULL)
@@ -25,6 +24,7 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	size1 = strlen(s1);
 	size2 = strlen(s2);
+	arr = malloc(sizeof(char) * (size1 + size2 + 1));
 
 	for (i = 0; i < (size1 + size2 + 1); i++)
 	{
