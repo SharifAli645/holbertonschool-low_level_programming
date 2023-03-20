5c9c1d723d8dcc715bc51444e818cd98600519c1
SharifAli645 <pandemiaonirica@gmail.com>
2023-03-20 11:40:11 -0700

    add an exit() if the first parameter is NULL

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 /**
  * init_dog - function that initialize a variable of type
@@ -12,6 +14,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if(d == NULL)
+	{
+		printf("Ok\n");
+		exit(0);
+	}
 
 	(*d).name = name;
 	(*d).age = age;
