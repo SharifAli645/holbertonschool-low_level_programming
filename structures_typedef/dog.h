1027c5151a6e5f3f802e76d1f595d9159c16ee70
SharifAli645 <pandemiaonirica@gmail.com>
2023-03-20 10:03:40 -0700

    structure for dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef ONE
+#define ONE
+/**
+ * struct dog - Short description
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: Longer description
+ **/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
