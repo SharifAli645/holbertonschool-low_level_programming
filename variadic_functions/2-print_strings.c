f90c475893804d10a8d2337f7d5f9dfe2819775c
SharifAli645 <pandemiaonirica@gmail.com>
2023-03-21 20:01:23 -0700

    header + function that prints strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - function that prints strings
+ * @separator: string to be printed
+ * @n: number of arguments
+ *
+ * Return: Always void
+ **/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned int i;
+
+	va_start(ptr, n);
+
+	if (separator == NULL)
+		separator = "";
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0 && i == (n - 1))
+			printf("%s", va_arg(ptr, char*));
+		else if (i == n - 1)
+			printf("%s", va_arg(ptr, char*));
+		else
+			printf("%s%s", va_arg(ptr, char*), separator);
+	}
+	va_end(ptr);
+	printf("\n");
+}
