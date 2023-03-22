59f4f8c492cd757efd17ae7a67f2909677cdd35e
SharifAli645 <pandemiaonirica@gmail.com>
2023-03-21 19:40:31 -0700

    header + function that prints numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - function that prints numbers
+ * @separator: string to be printed
+ * @n: number of arguments
+ *
+ * Return: Always void
+ **/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned int i;
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0 && i == (n - 1))
+			printf("%d", va_arg(ptr, int));
+		else if (i == n - 1)
+			printf("%d", va_arg(ptr, int));
+		else
+			printf("%d%s", va_arg(ptr, int), separator);
+	}
+	va_end(ptr);
+	printf("\n");
+}
