0306dee58000c6e8e0908126fcf53bd755487ec4
SharifAli645 <pandemiaonirica@gmail.com>
2023-03-22 17:13:19 -0700

    add if array or pointer to function is NULL return

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -4,12 +4,15 @@
  * as a parameter on each element of an array.
  * @size: size of the array
  * @action: pointer to function
- *
+ * @array: array
  * Return: Always void
  **/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
+	if (array == NULL || action == NULL)
+		return;
 
 	for (i = 0; i < size; i++)
 	{
