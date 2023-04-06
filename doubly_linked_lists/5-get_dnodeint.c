e0c7154a6635e8acb9c9e231fd28ac14bfb07318
SharifAli645 <pandemiaonirica@gmail.com>
2023-04-06 08:45:25 -0700

    feax: if index argument is bigger than the size of the list return NULL

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -11,6 +11,16 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int counter = 0;
 	dlistint_t *tmp = head;
 
+	while(tmp)
+	{
+		tmp = tmp->next;
+		counter++;
+	}
+	if(index > counter)
+		return (NULL);
+	counter = 0;
+	tmp = head;
+
 	while (index != counter)
 	{
 		tmp = tmp->next;
