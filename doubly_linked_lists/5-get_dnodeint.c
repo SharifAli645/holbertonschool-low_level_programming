f0fcb42a467b64fcf2bd63cc81d13a37e75fe2fd
SharifAli645 <pandemiaonirica@gmail.com>
2023-04-06 08:38:16 -0700

    a new function and its prototype

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - Function that returns the nth node of a list
+ * @head: Pointer to dlistint_t struct
+ * @index: Index
+ *
+ * Return: A node
+ **/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int counter = 0;
+	dlistint_t *tmp = head;
+
+	while (index != counter)
+	{
+		tmp = tmp->next;
+		counter++;
+	}
+	return (tmp);
+}
