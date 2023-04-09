451d0ab9278b91031d7a40945647a3d4dd0e99f7
SharifAli645 <pandemiaonirica@gmail.com>
2023-04-09 10:47:21 -0700

    new function and its prototype

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * set_bit - function that sets the value of a bit to 1 at a given index
+ * @n: number
+ * @index: index
+ *
+ * Return: 1 if it worked, -1 if an error occurred
+ **/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int ms = 1 << index;
+
+	*n = (*n & ~ms) | (1 << index);
+	return (1);
+}
