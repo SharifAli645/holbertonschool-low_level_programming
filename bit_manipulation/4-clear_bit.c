a83e7fcf3930821a68804ea4b05eb64e722eb64e
SharifAli645 <pandemiaonirica@gmail.com>
2023-04-09 11:03:55 -0700

    new function and its prototype

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * clear_bit - function that sets the value of a bit to 0 at a given index
+ * @n: pointer to decimal
+ * @index: index
+ *
+ * Return: 1 if it worked, -1 if an error occurred
+ **/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int ms = (1 << index);
+
+	*n = (*n & ~ms) | (0 << index);
+	return (1);
+}
