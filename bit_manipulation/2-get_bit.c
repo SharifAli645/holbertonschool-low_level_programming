860dde531d10c432222084e08a2941bf2f68c793
SharifAli645 <pandemiaonirica@gmail.com>
2023-04-09 10:23:08 -0700

    new function and its prototype

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_bit - function that returns the value of a bit at a given index
+ * @n: decimal number
+ * @index: index
+ *
+ * Return: the bit of the index
+ **/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int msk = 1 << index;
+
+	if (n & msk)
+		return (1);
+	else
+		return (0);
+}
