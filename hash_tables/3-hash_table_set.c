d97662591342f269f9c41d39f43c690c55f19e88
SharifAli645 <pandemiaonirica@gmail.com>
2023-04-14 17:07:27 -0700

    feax: update the value of the node is the new one has the same key

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,5 +1,47 @@
 #include "hash_tables.h"
 /**
+ * new_node - function that adds a node to list not empty
+ * @ht: table
+ * @key: key
+ * @value: value
+ * @index: index
+ *
+ * Return: 0 if it failed, 1 if it works
+ **/
+int new_node(hash_table_t *ht, const char *key, const char *value,
+		size_t index)
+{
+	hash_node_t *temp;
+	hash_node_t *ptr;
+
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		int e = strcmp(temp->key, key);
+
+		if (e == 0)
+		{
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
+	ptr = malloc(sizeof(hash_node_t));
+	if (!ptr)
+		return (0);
+	ptr->value = strdup(value);
+	if (!ptr->value)
+	{
+		free(ptr);
+		return (0);
+	}
+	ptr->key = (char *) key;
+	ptr->next = ht->array[index];
+	ht->array[index]->next = NULL;
+	ht->array[index] = ptr;
+	return (1);
+}
+/**
  * hash_table_set - tabloe
  * @ht: tab
  * @key: kk
@@ -10,9 +52,8 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	size_t index;
-	hash_node_t *ptr;
 
-	if (!key)
+	if (!key || !ht)
 		return (0);
 
 	index = key_index((const unsigned char *) key, ht->size);
@@ -32,19 +73,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index]->next = NULL;
 	} else
 	{
-		ptr = malloc(sizeof(hash_node_t));
-		if (!ptr)
+		if (!new_node(ht, key, value, index))
 			return (0);
-		ptr->value = strdup(value);
-		if (!ptr->value)
-		{
-			free(ptr);
-			return (0);
-		}
-		ptr->key = (char *) key;
-		ptr->next = ht->array[index];
-		ht->array[index]->next = NULL;
-		ht->array[index] = ptr;
 	}
 	return (1);
 }
