5466647418d535283f4d7c125b478bf852ca1bac
SharifAli645 <pandemiaonirica@gmail.com>
2023-04-15 21:42:47 -0700

    feax: only one malloc

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -9,26 +9,23 @@
  **/
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *ptr;
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
 
 	if (*head == NULL)
 	{
-		*head = malloc(sizeof(dlistint_t));
-		if (head == NULL)
-			return (NULL);
-		(*head)->n = n;
-		(*head)->prev = NULL;
-		(*head)->next = NULL;
+		new->next = NULL;
+		*head = new;
 	} else
 	{
-		ptr = malloc(sizeof(dlistint_t));
-		if (ptr == NULL)
-			return (NULL);
-		ptr->n = n;
-		ptr->prev = NULL;
-		(*head)->prev = ptr;
-		ptr->next = *head;
-		*head = ptr;
+		(*head)->prev = new;
+		new->next = *head;
+		*head = new;
 	}
 	return (*head);
 }
