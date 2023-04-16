d34b4fc763d05a87c89518c51f2582c509acf246
SharifAli645 <pandemiaonirica@gmail.com>
2023-04-16 16:24:16 -0700

    feax: readable

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,80 +1,62 @@
 #include "hash_tables.h"
 /**
- * new_node - function that adds a node to list not empty
- * @ht: table
- * @key: key
- * @value: value
- * @index: index
- *
- * Return: 0 if it failed, 1 if it works
+ * fr_node - Free one node
+ * @n: node
+ * Return: Always void
  **/
-int new_node(hash_table_t *ht, const char *key, const char *value,
-		size_t index)
+void fr_node(hash_node_t *n)
 {
-	hash_node_t *temp;
-	hash_node_t *ptr;
-
-	temp = ht->array[index];
-	while (temp != NULL)
-	{
-		int e = strcmp(temp->key, key);
-
-		if (e == 0)
-		{
-			temp->value = strdup(value);
-			return (1);
-		}
-		temp = temp->next;
-	}
-	ptr = malloc(sizeof(hash_node_t));
-	if (!ptr)
-		return (0);
-	ptr->value = strdup(value);
-	if (!ptr->value)
-	{
-		free(ptr);
-		return (0);
-	}
-	ptr->key = (char *) key;
-	ptr->next = ht->array[index];
-	ht->array[index]->next = NULL;
-	ht->array[index] = ptr;
-	return (1);
+	free(n->key);
+	free(n->value);
+	free(n);
 }
 /**
- * hash_table_set - tabloe
- * @ht: tab
- * @key: kk
- * @value: v
+ * hash_table_set -  function that adds an element to the hash table.
+ * @ht: hash table
+ * @key: key
+ * @value: value
  *
- * Return: 4;
+ * Return: 1 if it succeeded, 0 otherwise
  **/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	size_t index;
+	hash_node_t *n_node, *ptr;
 
-	if (!key || !ht)
+	if (key == NULL || ht == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *) key, ht->size);
-
+	n_node = malloc(sizeof(hash_node_t));
+	if (n_node == NULL)
+		return (0);
+	n_node->key = strdup(key);
+	n_node->value = strdup(value);
+	n_node->next = NULL;
 	if (ht->array[index] == NULL)
+		ht->array[index] = n_node;
+	else
 	{
-		ht->array[index] = malloc(sizeof(hash_node_t));
-		if (!ht->array[index])
-			return (0);
-		ht->array[index]->value = strdup(value);
-		if (!ht->array[index]->value)
+		ptr = ht->array[index];
+		if (strcmp(ptr->key, key) == 0)
 		{
-			free(ht->array[index]);
-			return (0);
+			n_node->next = ptr->next;
+			ht->array[index] = n_node;
+			fr_node(ptr);
+			return (1);
 		}
-		ht->array[index]->key = (char *) key;
-		ht->array[index]->next = NULL;
-	} else
-	{
-		if (!new_node(ht, key, value, index))
-			return (0);
+		while (ptr->next != NULL && strcmp(ptr->next->key, key) != 0)
+			ptr = ptr->next;
+		if (strcmp(ptr->key, key) == 0)
+		{
+			n_node->next = ptr->next->next;
+			fr_node(ptr->next);
+			ptr->next = n_node;
+		} else
+		{
+			n_node->next = ht->array[index];
+			ht->array[index] = n_node;
+		}
 	}
 	return (1);
 }
