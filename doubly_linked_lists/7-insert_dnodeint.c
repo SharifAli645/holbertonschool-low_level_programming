7c4f42f99e6b14ca1495f7a5b91a646de464c920
SharifAli645 <pandemiaonirica@gmail.com>
2023-04-15 22:12:57 -0700

    feax: use function 3-add..

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -49,6 +49,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (idx == 0)
 		return (add_dnodeint(h, n));
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
 	ptr->n = n;
 	ptr->prev = tmp->prev;
