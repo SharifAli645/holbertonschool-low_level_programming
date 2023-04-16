d51364736f9e266c901d0f95f1eeffcd5f858b86
SharifAli645 <pandemiaonirica@gmail.com>
2023-04-16 16:57:31 -0700

    header

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -34,8 +34,8 @@ hash_table_t *hash_table_create(unsigned long int size);
 hash_node_t *node(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 int new_node(hash_table_t *ht, const char *key, const char *value, size_t index);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
 #endif
