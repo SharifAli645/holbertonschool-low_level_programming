6e44f898192fc5e7a3ee279a9893ca32ed70a7c8
SharifAli645 <pandemiaonirica@gmail.com>
2023-04-16 07:57:08 -0700

    feax: empty string is text_content is NULL

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	if (!text_content)
+	if (text_content == NULL)
 		text_content = "";
 	while (*ptr != '\0')
 	{
