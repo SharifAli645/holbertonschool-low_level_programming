af4c7fab45559a9b42658f88c8f6037b2183193f
SharifAli645 <pandemiaonirica@gmail.com>
2023-04-16 15:12:34 -0700

    feax: node created

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -28,5 +28,5 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		ptr->next = new_node;
 		ptr->next->prev = ptr;
 	}
-	return (*head);
+	return (new_node);
 }
--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,26 +1,5 @@
 #include "lists.h"
 /**
- * counter - function that compare the index and a string
- * @h: list
- * @idx: index
- *
- * Return: 1 if the index is greater than the list
- **/
-int counter(dlistint_t *h, int idx)
-{
-	int cnt = 0;
-
-	while (h != NULL)
-	{
-		h = h->next;
-		cnt++;
-	}
-	if (idx >= cnt)
-		return (1);
-	else
-		return (0);
-}
-/**
  * insert_dnodeint_at_index - function that inserts a new node at a given
  * position
  * @h: Pointer to dlistint_t pointer
