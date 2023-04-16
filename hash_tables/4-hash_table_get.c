9ce94356d9789985ea69aa6e529c8f7b3dd5280d
SharifAli645 <pandemiaonirica@gmail.com>
2023-04-16 16:42:00 -0700

    feax: validate NULL cases

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -8,13 +8,21 @@
  **/
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int cnt = 0;
+	unsigned long int idx;
+	hash_node_t *ptr;
 
-	while(cnt < ht->size)
-	{
-		if(strcmp(ht->array[cnt]->key, key) == 0)
-			return(ht->array[cnt]->value);
-		cnt++;
-	}
-	return (NULL);
+	if (key == NULL || ht == NULL)
+		return (NULL);
+
+	idx = key_index((const unsigned char *) key, ht->size);
+	ptr = ht->array[idx];
+	if (ptr == NULL)
+		return (NULL);
+	while (strcmp(ptr->key, key) && ptr != NULL)
+		ptr = ptr->next;
+	if (ptr == NULL)
+		return (NULL);
+	else
+		return (ptr->value);
+
 }
