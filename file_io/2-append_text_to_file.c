b72c3e5946fc63847ead07a18455c8f18a00984a
SharifAli645 <pandemiaonirica@gmail.com>
2023-04-16 07:48:36 -0700

    feax: truncate if the file already exists

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		cnt++;
 		ptr++;
 	}
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND | O_TRUNC);
 	if (fd == -1)
 		return (-1);
 	write(fd, text_content, cnt);
